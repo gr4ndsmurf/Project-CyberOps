cae5ba015f4bb6020efe888795ffe387b313249b
Akın Haydaroğlu <gr4ndsmurf@gmail.com>
2023-08-01 01:51:50 +0300

    Weapon Shooting

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,13 +4,100 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject armPoint;
+    [SerializeField] private GameObject armPoint;
+
+    private const int POOL_SIZE = 10;
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePointTransform;
+    [SerializeField] private float bulletSpeed;
+
+    private Queue<GameObject> bulletPool;
+
+    [SerializeField] private string GunSoundName;
+
+    private bool shootingDelayed;
+    private void Start()
+    {
+        bulletPool = new Queue<GameObject>();
+
+        for (int i = 0; i < POOL_SIZE; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, Vector3.zero, Quaternion.identity);
+            bullet.SetActive(false);
+            bulletPool.Enqueue(bullet);
+        }
+    }
     void Update()
+    {
+        WeaponAiming();
+        WeaponShooting();
+    }
+
+    private void WeaponShooting()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (shootingDelayed == false)
+            {
+                StartCoroutine(shootingDelay());
+                shootingDelayed = true;
+                Vector3 mousePosition = Input.mousePosition;
+                Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                Vector3 shootDirection = (mouseWorldPosition - transform.position).normalized;
+
+                GameObject bullet = GetBulletFromPool();
+
+                if (bullet != null)
+                {
+                    bullet.transform.position = firePointTransform.position;
+                    bullet.transform.rotation = firePointTransform.rotation;
+                    bullet.SetActive(true);
+
+                    AudioManager.Instance.Play(GunSoundName);
+
+                    Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+                    bulletRigidbody.velocity = shootDirection * bulletSpeed;
+
+                    StartCoroutine(DisableBulletAfterDelay(bullet, 2f));
+                }
+            }
+        }
+    }
+    IEnumerator shootingDelay()
+    {
+        yield return new WaitForSeconds(0.25f);
+        shootingDelayed = false;
+    }
+    private IEnumerator DisableBulletAfterDelay(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ReturnBulletToPool(bullet);
+    }
+
+    private GameObject GetBulletFromPool()
+    {
+        if (bulletPool.Count > 0)
+        {
+            GameObject bullet = bulletPool.Dequeue();
+            bullet.SetActive(true);
+            return bullet;
+        }
+
+        return null;
+    }
+    private void ReturnBulletToPool(GameObject bullet)
+    {
+        bullet.SetActive(false);
+        bulletPool.Enqueue(bullet);
+    }
+
+    private void WeaponAiming()
     {
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         armPoint.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        // Silahý mirrorlama
+        //Flip
         Vector3 aimLocalScale = transform.localScale;
         Vector3 armPointLocalScale = armPoint.transform.localScale;
         if (angle > 90 || angle < -90)
@@ -30,6 +117,4 @@ public class Weapon : MonoBehaviour
         transform.localScale = aimLocalScale;
         armPoint.transform.localScale = armPointLocalScale;
     }
-
-
 }
