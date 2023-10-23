a8a8c31c476d821724c97b2e9b1e1de16963ae3d
Akın Haydaroğlu <gr4ndsmurf@gmail.com>
2023-10-24 02:22:48 +0300

    Lockpicking System Final

---
+++ Assets/Scripts/Puzzles/LockpickingPuzzles/Lockpicking.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Lockpicking : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+    private Image ballIMG;
+    private Color mainColor;
+
+    public enum LockpickStates { Stage_1, Stage_2, Stage_3, Stage_4, Completed}
+    public LockpickStates LockpickCurrentState = LockpickStates.Stage_1;
+
+    public bool completed = false;
+
+    [SerializeField] private GameObject canvas;
+
+    [SerializeField] private Image LockIMG;
+    [SerializeField] private Sprite[] stages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballIMG = ball.GetComponent<Image>();
+        mainColor = ballIMG.color;
+
+        ball.transform.DOLocalMoveX(500f, 2.0f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canvas.activeSelf == false)
+        {
+            ball.transform.DOPause();
+        }
+        else
+        {
+            ball.transform.DOPlay();
+
+            switch (LockpickCurrentState)
+            {
+                case LockpickStates.Stage_1:
+                    LockIMG.sprite = stages[0];
+                    if (ball.transform.localPosition.x > 250 && ball.transform.localPosition.x < 450)
+                    {
+                        ballIMG.color = Color.green;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Stage_2;
+                        }
+                    }
+                    else
+                    {
+                        ballIMG.color = mainColor;
+                    }
+                    break;
+                case LockpickStates.Stage_2:
+                    LockIMG.sprite = stages[1];
+                    if (ball.transform.localPosition.x > 0 && ball.transform.localPosition.x < 200)
+                    {
+                        ballIMG.color = Color.green;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Stage_3;
+                        }
+                    }
+                    else
+                    {
+                        ballIMG.color = mainColor;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Stage_1;
+                        }
+                    }
+                    break;
+                case LockpickStates.Stage_3:
+                    LockIMG.sprite = stages[2];
+                    if (ball.transform.localPosition.x < -50 && ball.transform.localPosition.x > -250)
+                    {
+                        ballIMG.color = Color.green;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Stage_4;
+                        }
+                    }
+                    else
+                    {
+                        ballIMG.color = mainColor;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Stage_2;
+                        }
+                    }
+                    break;
+                case LockpickStates.Stage_4:
+                    LockIMG.sprite = stages[3];
+                    if (ball.transform.localPosition.x > 0 && ball.transform.localPosition.x < 125)
+                    {
+                        ballIMG.color = Color.green;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Completed;
+                        }
+                    }
+                    else
+                    {
+                        ballIMG.color = mainColor;
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            LockpickCurrentState = LockpickStates.Stage_3;
+                        }
+                    }
+                    break;
+                case LockpickStates.Completed:
+                    LockIMG.sprite = stages[4];
+                    if (!completed)
+                    {
+                        Debug.Log("Lockpicking Completed");
+                        canvas.SetActive(false);
+                        ball.transform.DOKill(true);
+                        completed = true;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void CloseCanvas()
+    {
+        canvas.SetActive(false);
+    }
+}
