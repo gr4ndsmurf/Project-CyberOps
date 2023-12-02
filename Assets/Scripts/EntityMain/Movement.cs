fd01488ee3cc6ad984d1b86c3987b9b65597a5b0
Akın Haydaroğlu <gr4ndsmurf@gmail.com>
2023-12-02 18:59:43 +0300

    player movement bug fix

--- Assets/Scripts/EntityMain/Movement.cs
+++ Assets/Scripts/EntityMain/Movement.cs
@@ -12,7 +12,7 @@ public abstract class Movement : MonoBehaviour
     [SerializeField] protected float runningSpeed;
     [Tooltip("Default: 3.5")]
     [SerializeField] protected float jumpForce;
-    protected BoxCollider2D boxCollider2d;
+    protected CapsuleCollider2D capsuleCollider2D;
     protected Rigidbody2D rb;
 
     [SerializeField] protected LayerMask platformLayerMask;
@@ -20,14 +20,15 @@ public abstract class Movement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        boxCollider2d = GetComponent<BoxCollider2D>();
+        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
     public abstract void Move();
 
     protected bool IsGrounded()
     {
         float extraHeightText = 0.02f;
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size - new Vector3(0.1f, 0f, 0f), 0f, Vector2.down, extraHeightText, platformLayerMask);
+        RaycastHit2D raycastHit = Physics2D.CapsuleCast(capsuleCollider2D.bounds.center, capsuleCollider2D.bounds.size - new Vector3(0.1f, 0f, 0f), CapsuleDirection2D.Vertical, 0f, Vector2.down, extraHeightText, platformLayerMask);
+        
         Color rayColor;
         if (raycastHit.collider != null)
         {
@@ -37,9 +38,9 @@ public abstract class Movement : MonoBehaviour
         {
             rayColor = Color.red;
         }
-        Debug.DrawRay(boxCollider2d.bounds.center + new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
-        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
-        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, boxCollider2d.bounds.extents.y), Vector2.right * (boxCollider2d.bounds.extents.x), rayColor);
+        Debug.DrawRay(capsuleCollider2D.bounds.center + new Vector3(capsuleCollider2D.bounds.extents.x, 0), Vector2.down * (capsuleCollider2D.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(capsuleCollider2D.bounds.center - new Vector3(capsuleCollider2D.bounds.extents.x, 0), Vector2.down * (capsuleCollider2D.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(capsuleCollider2D.bounds.center - new Vector3(capsuleCollider2D.bounds.extents.x, capsuleCollider2D.bounds.extents.y), Vector2.right * (capsuleCollider2D.bounds.extents.x), rayColor);
         //Debug.Log(raycastHit.collider);
         return raycastHit.collider != null;
     }
